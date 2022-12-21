718dd9a10107e821b6bef0a19cc8824b24fb4432
souvik-wizard <dassouvik3327@gmail.com>
2022-12-22 02:03:18 +0530

    New

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,20 @@
+var randomNumber1=Math.floor(Math.random()*6)+1
+var randomImage1="images/dice"+randomNumber1+".png"
+
+var randomNumber2=Math.floor(Math.random()*6)+1
+var randomImage2="images/dice"+randomNumber2+".png"
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImage1)
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2)
+
+
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player1 Wins🚩"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player2 Wins🚩"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw🏳️"
+
+}
---
+++ Drum Kit/index.js
@@ -0,0 +1,6 @@
+let len=document.querySelectorAll(".drum").length;
+for (let i=0;i<len;i++ ){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        // alert("clicked")
+        
+})}
