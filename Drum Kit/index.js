98fa95b0322981e08afd121167ae97fc60604061
souvik-wizard <dassouvik3327@gmail.com>
2022-12-30 14:55:40 +0530

    Update index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -2,6 +2,6 @@ let len=document.querySelectorAll(".drum").length;
 for (let i=0;i<len;i++ ){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
         this.style.color="White"
-        // var audio=new Audio('sounds/crash.mp3');  
-        // audio.play();
-})}
+    })}
+    // var audio=new Audio('sounds/crash.mp3');  
+    // audio.play();
