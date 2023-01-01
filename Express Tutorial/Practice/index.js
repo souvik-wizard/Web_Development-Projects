81cacbb5847450ec971ffb4d88192b330ea7004d
souvik-wizard <dassouvik3327@gmail.com>
2023-01-02 00:57:01 +0530

    .

--- Express Tutorial/Practice/index.js
+++ Express Tutorial/Practice/index.js
@@ -1,7 +1,12 @@
 const express=require("express")
 const app = express()
+
 app.get("/",function(req,res){
     res.send("now you see mee")
+})
+
+app.get("/signin",function(req,res){
+    res.send("you can sign in here now")
 })
 app.listen(3000,function(){
     console.log("Our port is running")
