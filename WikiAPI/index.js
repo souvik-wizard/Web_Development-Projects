bff8dd0655a2cb54ca77537251c67a1c3b155c3e
souvik-wizard <dassouvik3327@gmail.com>
2023-03-23 00:03:55 +0530

    Update index.js

--- WikiAPI/index.js
+++ WikiAPI/index.js
@@ -1,14 +1,14 @@
-const mongoose=require('mongoose');
-const bodyParser=require('body-parser');
-const express=require('express');
-const ejs=require('ejs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const express = require('express');
+const ejs = require('ejs');
 require('dotenv').config();
 
 
-const app=express();
+const app = express();
 
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 let wikiDB = async () => {
@@ -17,6 +17,30 @@ let wikiDB = async () => {
 }
 wikiDB();
 
-app.listen(80,function(){
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", async function (req, res) {
+    const articles = await Article.find()
+    res.send(articles);
+});
+
+app.post("/articles",async function (req, res) {
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    await newArticle.save();
+    res.redirect("/articles");
+});
+
+app.listen(80, function () {
     console.log("Server started on port 3000");
 });
